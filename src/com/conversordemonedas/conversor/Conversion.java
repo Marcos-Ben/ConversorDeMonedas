6a5baf7aeb6e505fc8385a7bf9dab8cff6930e19
Marcos Benencia <106993562+Marcos-Ben@users.noreply.github.com>
2024-10-10 19:31:33 -0300

    private variables

--- src/com/conversordemonedas/conversor/Conversion.java
+++ src/com/conversordemonedas/conversor/Conversion.java
@@ -1,11 +1,11 @@
 package com.conversordemonedas.conversor;
 
 public class Conversion {
-    String fromCurrency;
-    String toCurrency;
-    double originalAmount;
-    double convertedAmount;
-    double conversionRate;
+    private String fromCurrency;
+    private String toCurrency;
+    private double originalAmount;
+    private double convertedAmount;
+    private double conversionRate;
 
     public Conversion(String fromCurrency, String toCurrency, double originalAmount,
                       double convertedAmount, double conversionRate) {
@@ -14,5 +14,25 @@ public class Conversion {
         this.originalAmount = originalAmount;
         this.convertedAmount = convertedAmount;
         this.conversionRate = conversionRate;
+    }
+
+    public String getFromCurrency() {
+        return fromCurrency;
+    }
+
+    public String getToCurrency() {
+        return toCurrency;
+    }
+
+    public double getOriginalAmount() {
+        return originalAmount;
+    }
+
+    public double getConvertedAmount() {
+        return convertedAmount;
+    }
+
+    public double getConversionRate() {
+        return conversionRate;
     }
 }
